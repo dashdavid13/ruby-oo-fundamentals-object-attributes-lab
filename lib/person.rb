b008c6ef6b27334110206a644fa6041a7605ca5a
dashdavid13 <davidamedina619@gmail.com>
2020-10-26 22:42:50 -0400

    done

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+    def name(person_name)
+        @name = person_name
+    end 
+
+
+
+    
+    def name 
+        @name 
+    end 
+    def name= (new_name)
+        @name = new_name
+    end 
+
+    def job(job_details)
+        @job = job_details
+    end 
+
+    def job
+        @job
+    end 
+
+    def job= (new_job_details)
+        @job = new_job_details
+    end 
+end 
